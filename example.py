ccc11e3ed6f9634054df49d3da43df8971cb2741
Andy Aylward <aaylward@gmail.com>
2017-11-18 16:20:31 -0500

    :do_not_litter:

--- example.py
+++ example.py
@@ -14,11 +14,11 @@ def say_hi(request):
     return Response(200, {'message': 'hi ' + request.qs.get('name', ['friend'])[0]})
 
 def main():
-    router = Router().\
-            with_route(Resource('', 'GET'), lambda req: Response(200, 'sup', {'Content-Type': 'text/plain'})).\
-            with_route(Resource('/p', 'POST'), handle_post).\
-            with_route(Resource('/err', 'GET'), lambda req: 1/0).\
-            with_route(Resource('/hi', 'GET'), say_hi)
+    router = Router() \
+            .with_route(Resource('', 'GET'), lambda req: Response(200, 'sup', {'Content-Type': 'text/plain'})) \
+            .with_route(Resource('/p', 'POST'), handle_post) \
+            .with_route(Resource('/err', 'GET'), lambda req: 1/0) \
+            .with_route(Resource('/hi', 'GET'), say_hi)
     server = Server(router=router)
     server.run()
 
